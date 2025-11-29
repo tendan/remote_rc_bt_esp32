90e2e5943c0cc5a53ac0ca720f717b1ffa44d0bf
Maciej Ślusarz <maciej.slusarz@proton.me>
2025-11-30 00:05:42 +0100

    Next steps with motor refactors (make it compatible with SOLID)

--- src/hardware/motor.rs
+++ src/hardware/motor.rs
@@ -6,140 +6,162 @@ use esp_hal::gpio::Level;
 use esp_hal::gpio::Output;
 use esp_hal::mcpwm::McPwm;
 use esp_hal::mcpwm::PwmPeripheral;
+use esp_hal::DriverMode;
 use log::info;
 
-pub trait Motor<'a> {}
+pub trait MotorDriver {
+    fn set_power(&mut self, value: u8);
+}
+
+pub enum MotorVariant<B, P> {
+    Binary(B),
+    Pwm(P),
+}
+
+impl<B, P> MotorDriver for MotorVariant<B, P>
+where
+    B: MotorDriver,
+    P: MotorDriver,
+{
+    fn set_power(&mut self, power: u8) {
+        match self {
+            Self::Binary(b) => b.set_power(power),
+            Self::Pwm(p) => p.set_power(power),
+        }
+    }
+}
 
 // =============================
 // TODO: DO NOT REMOVE THIS, IT MIGHT BE REALLY GOOD IDEA
 pub struct BinaryMotor<'a> {
-    pub test: Output<'a>,
+    pub motor: Output<'a>,
 }
 pub struct PwmMotor<'a, PWM> {
-    pub test: McPwm<'a, PWM>,
+    pub motor: McPwm<'a, PWM>,
 }
 
-impl<'a> Motor<'a> for BinaryMotor<'a> {}
+impl<'a> MotorDriver for BinaryMotor<'a> {
+    fn set_power(&mut self, value: u8) {
+        // TODO
+    }
+}
 
-impl<'a, PWM: PwmPeripheral> Motor<'a> for PwmMotor<'a, PWM> {}
+impl<'a, PWM: PwmPeripheral> MotorDriver for PwmMotor<'a, PWM> {
+    fn set_power(&mut self, value: u8) {
+        // TODO
+    }
+}
 // =============================
 
 // =============================
-// TODO: DO NOT REMOVE THIS, IT MIGHT BE REALLY GOOD IDEA
-pub trait SteeringAxle {}
+pub trait SteeringAxle {
+    fn set_steering(&mut self, value: u8);
+}
 
-pub trait Accelerator {}
+pub trait Accelerator {
+    fn set_throttle(&mut self, value: u8);
+}
 
-pub struct BinarySteeringAxle {}
+pub struct BinarySteeringAxle<M>
+where
+    M: MotorDriver,
+{
+    pub motor_left: M,
+    pub motor_right: M,
+}
 
-pub struct ServoSteeringAxle {}
+impl<M: MotorDriver> SteeringAxle for BinarySteeringAxle<M> {
+    fn set_steering(&mut self, value: u8) {
+        // TODO
+    }
+}
 
-pub struct BinaryAccelerator {}
+pub struct ServoSteeringAxle<M> {
+    pub motor_steer: M,
+}
+
+impl<M: MotorDriver> SteeringAxle for ServoSteeringAxle<M> {
+    fn set_steering(&mut self, value: u8) {
+        // TODO
+    }
+}
+
+pub struct BinaryAccelerator<MF, MB> {
+    pub motor_forward: MF,
+    pub motor_backward: MB,
+}
+
+impl<MF: MotorDriver, MB: MotorDriver> Accelerator for BinaryAccelerator<MF, MB> {
+    fn set_throttle(&mut self, value: u8) {
+        // TODO
+    }
+}
 
 pub struct LinearAccelerator {}
+
+impl Accelerator for LinearAccelerator {
+    fn set_throttle(&mut self, value: u8) {
+        // TODO
+    }
+}
 // =============================
 
-pub struct MotorSetup<'a> {
-    pub(super) accelerator: A,
-    pub(super) backmove: B,
-    pub(super) steer_left: L,
-    pub(super) steer_right: R,
+pub trait MotorSetup {
+    fn moves(&mut self) -> bool;
+    fn stop(&mut self) -> bool;
 }
 
-pub struct Motors<'a> {
-    motor_setup: MotorSetup<'a>,
+pub struct RobotChassis<A, S> {
+    pub accelerator: A,
+    pub steering: S,
 }
 
-pub enum MotorsStatus {
-    Steady,
-    Moving,
-}
-
-impl From<bool> for MotorsStatus {
-    fn from(value: bool) -> Self {
-        if value {
-            MotorsStatus::Moving
-        } else {
-            MotorsStatus::Steady
+impl<A, S> RobotChassis<A, S>
+where
+    A: Accelerator,
+    S: SteeringAxle,
+{
+    pub fn new(accelerator: A, steering: S) -> Self {
+        Self {
+            accelerator,
+            steering,
         }
     }
 }
 
-pub enum MotorAddress {
-    Stop,
-    Accelerate,
-    Back,
-    Steer,
-    UnknownAddress,
-}
-
-impl From<u8> for MotorAddress {
-    fn from(value: u8) -> Self {
-        match value {
-            0x0 => MotorAddress::Stop,
-            0x1 => MotorAddress::Accelerate,
-            0x2 => MotorAddress::Back,
-            0x3 => MotorAddress::Steer,
-            _ => MotorAddress::UnknownAddress,
-        }
+impl<A, M> MotorSetup for RobotChassis<A, BinarySteeringAxle<M>>
+where
+    A: Accelerator,
+    M: MotorDriver,
+{
+    fn moves(&mut self) -> bool {
+        true //TODO
+    }
+    fn stop(&mut self) -> bool {
+        true //TODO
     }
 }
 
-impl<'a> AddressablePeripheral<'a, MotorsStatus /*, MotorAddress*/> for Motors<'a> {
-    fn perform_function(
-        self: &mut Self,
-        function_code: u8,
-        address: u8,
-        value: u8,
-    ) -> Result<MotorsStatus, PerformFunctionError> {
-        match function_code {
-            0x0 => {
-                info!("Waiting for commands...");
-                Ok(MotorsStatus::from(self.moves()))
-            }
-            0x1 => self.perform_action(MotorAddress::from(address), value),
-            _ => Err(PerformFunctionError::WrongFunctionCode),
-        }
+impl<A, M> MotorSetup for RobotChassis<A, ServoSteeringAxle<M>>
+where
+    A: Accelerator,
+    M: MotorDriver,
+{
+    fn moves(&mut self) -> bool {
+        true //TODO
+    }
+    fn stop(&mut self) -> bool {
+        true //TODO
     }
 }
 
-impl<'a> Motors<'a> {
-    pub fn setup(motor_setup: MotorSetup<'a>) -> Self {
-        Self { motor_setup }
-    }
+pub struct Motors<C> {
+    chassis: C,
+}
 
-    pub fn moves(self: &Self) -> bool {
-        return self.motor_setup.accelerator.is_set_high()
-            || self.motor_setup.backmove.is_set_high()
-            || self.motor_setup.steer_left.is_set_high()
-            || self.motor_setup.steer_right.is_set_high();
-    }
-
-    fn accelerate(self: &mut Self, enable: Level) {
-        self.motor_setup.backmove.set_low();
-        self.motor_setup.accelerator.set_level(enable);
-    }
-
-    fn backmove(self: &mut Self, enable: Level) {
-        self.motor_setup.accelerator.set_low();
-        self.motor_setup.backmove.set_level(enable);
-    }
-
-    fn go_left(self: &mut Self, enable: Level) {
-        self.motor_setup.steer_right.set_low();
-        self.motor_setup.steer_left.set_level(enable);
-    }
-
-    fn go_right(self: &mut Self, enable: Level) {
-        self.motor_setup.steer_left.set_low();
-        self.motor_setup.steer_right.set_level(enable);
-    }
-
-    pub fn stop(self: &mut Self) {
-        self.motor_setup.accelerator.set_low();
-        self.motor_setup.backmove.set_low();
-        self.motor_setup.steer_left.set_low();
-        self.motor_setup.steer_right.set_low();
+impl<C> Motors<C> {
+    pub fn new(chassis: C) -> Self {
+        Self { chassis }
     }
 
     fn perform_action(
@@ -190,6 +212,64 @@ impl<'a> Motors<'a> {
                 Ok(MotorsStatus::from(self.moves()))
             }
             MotorAddress::UnknownAddress => Err(PerformFunctionError::IncorrectAddress),
+        }
+    }
+}
+
+pub enum MotorsStatus {
+    Steady,
+    Moving,
+}
+
+impl From<bool> for MotorsStatus {
+    fn from(value: bool) -> Self {
+        if value {
+            MotorsStatus::Moving
+        } else {
+            MotorsStatus::Steady
+        }
+    }
+}
+
+pub enum MotorAddress {
+    Stop,
+    Accelerate,
+    Back,
+    Steer,
+    UnknownAddress,
+}
+
+impl From<u8> for MotorAddress {
+    fn from(value: u8) -> Self {
+        match value {
+            0x0 => MotorAddress::Stop,
+            0x1 => MotorAddress::Accelerate,
+            0x2 => MotorAddress::Back,
+            0x3 => MotorAddress::Steer,
+            _ => MotorAddress::UnknownAddress,
+        }
+    }
+}
+
+impl<'a, A, S> AddressablePeripheral<'a, MotorsStatus /*, MotorAddress*/>
+    for Motors<RobotChassis<A, S>>
+where
+    A: Accelerator,
+    S: SteeringAxle,
+{
+    fn perform_function(
+        self: &mut Self,
+        function_code: u8,
+        address: u8,
+        value: u8,
+    ) -> Result<MotorsStatus, PerformFunctionError> {
+        match function_code {
+            0x0 => {
+                info!("Waiting for commands...");
+                Ok(MotorsStatus::from(self.moves()))
+            }
+            0x1 => self.perform_action(MotorAddress::from(address), value),
+            _ => Err(PerformFunctionError::WrongFunctionCode),
         }
     }
 }
